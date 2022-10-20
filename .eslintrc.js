7d93066c31ed59e87b6006db1273a752385e47e3
ebulsok <leebulsok@gmail.com>
2022-10-20 19:44:43 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 'off',
+    'prefer-destructuring': 'off',
+    'no-plusplus': 'off',
+  },
+};
