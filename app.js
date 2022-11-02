a7013b31b77d463e9478cecf8af2fc6a52a3516c
ebulsok <leebulsok@gmail.com>
2022-11-03 01:57:17 +0900

    ebulsok/글_NoUsers: 안 쓰는 파일 삭제, DB 설계 변경사항 업데이트

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ const express = require('express');
 const cors = require('cors');
 
 require('dotenv').config();
-const session = require('express-session');
-const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT;
 
 const router = require('./routes/index');
-const loginRouter = require('./routes/login');
-const passportRouter = require('./routes/passport');
 const postsRouter = require('./routes/posts');
-
-passportRouter();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,18 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cors());
 
-app.use(
-  session({
-    secret: 'secret key',
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use('/', router);
-app.use('/auth', loginRouter.router);
 app.use('/posts', postsRouter);
 
 app.use((err, req, res, next) => {
