4ebbce2c4edff0bc2ad68f7a8c05c88ddd9bef87
ebulsok <96829798+ebulsok@users.noreply.github.com>
2022-12-16 14:28:28 +0900

    chore: Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['airbnb-base'],
-  rules: {
-    'linebreak-style': 0,
-    'no-console': 'off',
-    'prefer-destructuring': 'off',
-    'no-plusplus': 'off',
-  },
-};
